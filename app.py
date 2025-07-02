3746623b232243b90909227c53f973135ae8a103
Ungnii <ekbp0120@gmail.com>
2025-07-02 16:14:41 +0900

    Update app.py

--- app.py
+++ app.py
@@ -119,4 +119,5 @@ def admin_login():
 # 시작 시 DB 초기화
 if __name__ == '__main__':
     init_db()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
